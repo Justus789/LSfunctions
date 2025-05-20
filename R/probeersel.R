840662f10decec46345544d5810bcb79eddad582
Justus Bijen <justus.bijen@student.hu.nl>
2025-05-20 21:54:13 +0200

    4e function files() is af

---
+++ R/probeersel.R
@@ -0,0 +1,46 @@
+
+#' comparing
+#'
+#' @param x either "kort" or "lang"
+#' @param dir_path a path from which you want the list of folders
+#'
+#' @return A character string with information about working directory and a list with the folder structure
+#' @export
+#'
+#' @examples
+#' files("kort", "/tests/")
+#' files("lang")
+files <- function(x, dir_path = NA) {
+  if (x == "kort") {
+    print("Dit is de working directory")
+    print(getwd())
+    if (is.na(dir_path)) {
+      print("Hieronder de lijst met je files")
+      print(list.files(path = ".", recursive = TRUE, full.names = TRUE))
+    }
+    else {
+      print(paste("vanaf", paste0(getwd(), dir_path), "gaat de structuur zo"))
+      print(list.files(path = paste0(getwd(), dir_path), recursive = TRUE))
+    }
+  }
+  if (x == "lang") {
+    print("Dit is de working directory")
+    print(getwd())
+    if (is.na(dir_path)) {
+      print("Hieronder de lijst met je files")
+      print(list.files(path = ".", recursive = TRUE, full.names = TRUE))
+    }
+    else {
+      print(paste("vanaf", dir_path, "gaat de structuur zo"))
+      print(list.files(path = paste0(getwd(), dir_path), recursive = TRUE, full.names = TRUE))
+    }
+  }
+  if (is.na(x)) {
+    # If an invalid argument is passed
+    stop("Invalid option. Please use 'kort' or 'lang'.")
+  }
+}
+
+#files("kort", "/tests/")
+#files("lang")
+#files("lang", "/tests/")
